f784536e9554e7cf8e61fefe76b7e15badc212a9
Brandon Leichty <brandonl@me.com>
2017-01-25 15:32:18 -0600

    Added description page/back button
    When a movie poster is clicked it hides all the movies/list items and
    appends html for a movie description page. When the “Search Results”
    button is clicked it un-hides the movies that were populated from the
    user search

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ var userMovieYearInput;
 var OMDb_URL = 'http://www.omdbapi.com/?';
 
 
+
+
 //Start of movie search/submit button
 $('#submit').on('click', function(event){
   //Look more into specifics of how this works
@@ -15,8 +17,11 @@ $('#submit').on('click', function(event){
   //Set year variable
   userMovieYearInput = $('#year').val();
 
+  $('#descriptionBackground').remove();
+
   //Clears the search results
   $('#movies').empty();
+
 
   //Get user entered movie title and trims it
   userMovieSearchInput = $('#search').val().trim();
@@ -38,10 +43,43 @@ $('#submit').on('click', function(event){
 }); //End of #submit click event handler function
 
 
+
+
+
 $('ul').on('click', 'li', function(){
   $('#movies').find('li').hide();
+
+  // var html = '<div class="description-background" id="descriptionBackground">' +
+  //               '<div class="back-search" id="backSearchLnk">' +
+  //                 '<span class="back-search-link"><i class="material-icons">chevron_left</i>Search</span>' +
+  //               '</div>' +
+  //            '</div>';
+
+    var html =   '<div class="description-background" id="descriptionBackground">' +
+                  '<div class="back-search-link back-search" id="backSearchLnk">' +
+                    '<i class="material-icons chevron-left">chevron_left</i>Search Results' +
+                  '</div>' +
+               '</div>';
+
+               console.log("The movie clicked is: " + this.moviePropertie.Title)
+
+  $('#main-header').after(html);
+
   console.log("THIS WORKED!");
 });
+
+
+
+
+
+$('body').on('click', '#backSearchLnk', function(){
+    $('#descriptionBackground').remove();
+    console.log("Go back to original search");
+    $('#movies').find('li').fadeIn();
+});
+
+
+
 
 
  function displayMoviesCallback(movie) {
@@ -61,7 +99,8 @@ $('ul').on('click', 'li', function(){
           if(moviePropertie.Poster === "N/A"){
             searchResults += '<i class="material-icons poster-placeholder">crop_original</i></div>';
           } else {
-            searchResults += '<a href="' + imdbHTML + '" target="_blank"><img class="movie-poster" src=' + moviePropertie.Poster + '></div></a>';
+                            //'<a href="' + imdbHTML + '" target="_blank"><img class="movie-poster" src=' + moviePropertie.Poster + '></div></a>';
+            searchResults += '<img class="movie-poster" src=' + moviePropertie.Poster + '></div>';
           }
 
           searchResults +=          '<span class="movie-title">' + moviePropertie.Title + '</span>' +
@@ -83,5 +122,4 @@ $('ul').on('click', 'li', function(){
        return noMovieFound;
    });
   }
-}
-//END of displayMoviesCallback
+} //END of displayMoviesCallback
